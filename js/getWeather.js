a7ad9fdc2bdec16f8754fb3adb5b8dcb7cc642d7
Luiz Henrique <luiz.heenrike7@gmail.com>
2025-03-31 20:44:41 -0300

    Adicionando o Js.

--- js/getWeather.js
+++ js/getWeather.js
@@ -1,2 +1,50 @@
 "use strict";
+import { apiKey } from "../js/apiKey.js";
 
+export async function getWeather() {
+  const city = document.getElementById("city").value.trim();
+
+  if (!city) {
+    alert("Please, enter a city.");
+    return;
+  }
+
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(
+    city
+  )}?unitGroup=metric&key=${apiKey}&contentType=json`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Error fetching data from API");
+    }
+
+    const data = await response.json();
+    displayWeather(data);
+  } catch (error) {
+    console.log(error);
+    alert("Deu erro");
+  }
+}
+
+export function displayWeather(data) {
+  const weatherResponse = document.getElementById("weather-response");
+
+  if (data.currentConditions) {
+    const { temp, feelslike, humidity, precip, conditions, windspeed } =
+      data.currentConditions;
+
+    weatherResponse.innerHTML = `
+  <h3>Weather in ${data.address}</h3>
+      <p><strong>Current Temperature:</strong> ${temp}°C</p>
+      <p><strong>Feels Like:</strong> ${feelslike}°C</p>
+      <p><strong>Humidity:</strong> ${humidity}%</p>
+      <p><strong>Precipitation:</strong> ${precip} mm</p>
+      <p><strong>Conditions:</strong> ${conditions}</p>
+      <p><strong>Wind Speed:</strong> ${windspeed} km/h</p>
+  `;
+  } else {
+    weatherResponse.innerHTML = "<p>Fudeu</p>";
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
 "use strict";
 
+/* Imports */
+import { apiKey } from "./apiKey.js";
+import { getWeather } from "./getWeather.js";
+
+/* HTML Elements */
+const getStartedBtn = document.getElementById("getStartedBtn");
+const getWeatherBtn = document.getElementById("getWeatherBtn");
+
+getStartedBtn.addEventListener("click", () => {
+  document.getElementById("city").focus();
+});
+
+getWeatherBtn.addEventListener("click", getWeather);
